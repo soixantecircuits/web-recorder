a92f5a3e5a3aacd67e403f50887fe4ab9ec778a6
gabrielstuff <gabriel@soixantecircuits.fr>
2015-01-05 23:56:49 +0100

    adding websocket recorder available thru webpage

--- app.js
+++ app.js
@@ -1,49 +1,74 @@
+'use strict';
+
 var fs = require('fs'),
   FfmpegCommand = require('fluent-ffmpeg'),
   command = new FfmpegCommand(),
   frame = 0,
   config = require('config.json')('./config/default.json'),
-  io = require('socket.io')(config.socketio.port),
   Stopwatch = require('timer-stopwatch'),
   timer = new Stopwatch(config.timer.duration),
-  recording = false;
+  recording = false,
+  io,
+  totalDuration = config.timer.duration,
+  app = {};
 
-timer.on('time', function(time) {
-  console.log(time.ms); // number of milliseconds past (or remaining);
-  io.emit('time', {
-    time: time.ms
-  });
-});
-
-// Fires when the timer is done
-timer.on('done', function() {
-  console.log('Timer is complete');
-  makeMovie();
-  stop();
-  io.emit('stop', 'timer done');
-});
-
-// Fires when the timer is almost complete - default is 10 seconds remaining. Change with 'almostDoneMS' option
-timer.on('almostdone', function() {
-  console.log('Timer is almost complete');
-});
-
-io.on('connection', function(socket) {
-  console.log('some client connect...');
-  socket
-    .on('frame', function(img) {
-      if (recording) {
-        saveImage(img);
-      }
-    })
-    .on('start', function() {
-      recording = true;
-      timer.start();
-    })
-    .on('stop', function() {
-      stop();
+var initTimerHandler = function() {
+  timer.on('time', function(time) {
+    console.log(time.ms); // number of milliseconds past (or remaining);
+    io.emit('time', {
+      time: time.ms,
+      duration: totalDuration
     });
-});
+  });
+
+  // Fires when the timer is done
+  timer.on('done', function() {
+    console.log('Timer is complete');
+    makeMovie();
+    stop();
+    io.emit('stop', 'timer done');
+  });
+
+  // Fires when the timer is almost complete - default is 10 seconds remaining. Change with 'almostDoneMS' option
+  timer.on('almostdone', function() {
+    console.log('Timer is almost complete');
+  });
+}
+
+var initStatiqueServer = function() {
+  var Statique = require("statique");
+
+  // Create *Le Statique* server
+  var server = new Statique({
+    root: __dirname + "/public",
+    cache: 36000
+  }).setRoutes({
+    "/": "/html/index.html"
+  });
+
+  // Create server
+  app = require('http').createServer(server.serve);
+}
+
+var initSocketioServer = function() {
+  io = require('socket.io')(app);
+  io.on('connection', function(socket) {
+    console.log('some client connect...');
+    socket
+      .on('frame', function(img) {
+        if (recording) {
+          saveImage(img);
+        }
+      })
+      .on('start', function() {
+        recording = true;
+        timer.start();
+      })
+      .on('stop', function() {
+        stop();
+      });
+  });
+}
 
 var saveImage = function(img) {
   console.log("Saving...")
@@ -68,9 +93,22 @@ var makeMovie = function() {
     .output('outputfile.mp4')
     .on('end', function(data) {
       console.log('Finished processing: ', data);
+      io.emit('success', {
+        msg:'Video saved'
+      });
     })
-    .on('error', function(err){
+    .on('error', function(err) {
       console.error(err);
+      io.emit('error', {
+        msg:'Video has not been saved'
+      });
     })
     .run();
 }
+
+// Output
+console.log("Listening on: http://localhost:" + config.server.port);
+initStatiqueServer();
+initSocketioServer();
+initTimerHandler();
+app.listen(config.server.port);
